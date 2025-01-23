1ee3395ac3f9376be63b919431ca2527fa8d9755
Wisdom Okogho <wisdommokoghoo@gmail.com>
2025-01-23 19:48:21 +0100

    Sign up route created successfully

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 dotenv.config();
 
 mongoose
@@ -14,6 +16,20 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("server is running on port 3000");
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/user", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
