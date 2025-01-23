e2881360143e11c0a3230d8cd19f5e9908d8fa9c
Wisdom Okogho <wisdommokoghoo@gmail.com>
2025-01-23 11:22:20 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch(() => {
+    console.log("error seen");
+  });
 
 const app = express();
 
