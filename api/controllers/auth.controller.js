6a04b3f5f22a579c857464e19b3a3c61ad401939
Wisdom Okogho <wisdommokoghoo@gmail.com>
2025-01-24 10:12:22 +0100

    Delete api route created successfully

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -32,3 +32,19 @@ export const signin = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    await User.findByIdAndDelete(userId);
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,9 +1,11 @@
-import { signin, signup } from "../controllers/auth.controller.js";
+import { deleteUser, signin, signup } from "../controllers/auth.controller.js";
 import express from "express";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
